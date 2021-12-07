58a9b43b1d0d21f5058a72b6f9cccf066ddb40ea
saemhco <saulem1994@gmail.com>
2021-12-07 11:23:24 -0500

    v2.5.2 para manual

--- js/chunk-2d0b3a2d.e0dc38db.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b3a2d"],{"28e6":function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("HOLAS")])},c=[],l={},a=l,r=t("2877"),s=Object(r["a"])(a,u,c,!1,null,null,null);e["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0b3a2d.e0dc38db.js.map
